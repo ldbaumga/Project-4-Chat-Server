3a1fda73dcbf3cdd0ceca2d47f1a400ff837bfa1
hzemy <42785829+hzemy@users.noreply.github.com>
2018-11-19 16:13:44 -0500

    Updated ChatMessage.java

--- ChatMessage.java
+++ ChatMessage.java
@@ -6,4 +6,44 @@ final class ChatMessage implements Serializable {
 
     // Here is where you should implement the chat message object.
     // Variables, Constructors, Methods, etc.
+
+    private String message;
+    private int type;
+    private String recipient;
+    private boolean isListMessage;
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public ChatMessage(String message, int type) {
+        this.message = message;
+        this.type = type;
+        if(message.startsWith("/msg")) {
+            recipient = message.substring(5, message.indexOf(" ", 5));
+        }
+        if (message.startsWith("/list")) {
+          isListMessage = true;
+        } else {
+            isListMessage = false;
+        }
+    }
+
+    public ChatMessage(String message, int type, String recipient) {
+        this(message,type);
+        if(message.startsWith("/msg")) {
+            recipient = message.substring(5, message.indexOf(" ", 5));
+        }
+        this.recipient = recipient;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
 }
